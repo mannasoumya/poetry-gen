4354bb644813e4af24227cefab63aacf01040ba3
mannasoumya <sm323265@gmail.com>
2021-03-25 20:55:56 +0530

    BAIC UI presented

--- data/do.py
+++ data/do.py
@@ -1,15 +1,22 @@
-import random
-file_str=""
-with open("Rupi Kaur - Milk and Honey-Andrews McMeel Publishing (2015).txt",'r',encoding='utf-8') as f:
-    file_str = f.read()
+# import random
+# file_str=""
+# with open("Rupi Kaur - Milk and Honey-Andrews McMeel Publishing (2015).txt",'r',encoding='utf-8') as f:
+#     file_str = f.read()
 
-f.close()
+# f.close()
 
-all_lines=file_str.split("\n")
-for line in all_lines:
-    if line== '\r':
-        all_lines.remove(line)
+# all_lines=file_str.split("\n")
+# for line in all_lines:
+#     if line== '\r':
+#         all_lines.remove(line)
 
-print(all_lines)
+# print(all_lines)
 # all_lines_str=""
 # with open()
+
+file_str = open('r_p_poem.txt','r').read()
+
+file_str = file_str.replace("qytyt","\n")
+
+with open('r_p_poem2.txt','w') as f:
+    f.write(file_str)
